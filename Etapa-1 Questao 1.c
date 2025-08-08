a3682b26f7b51e704a1ae3f6bd61a6dca0a59818
Afonso <afonsobranco34@outlook.com>
2025-08-07 22:29:57 -0300

    Add files via upload

---
+++ Etapa-1 Questao 1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Afonso Branco\n");
+    printf("19 anos\n");
+    printf("Liciatura em Computação\n");
+    return 0;
+}
---
+++ Etapa-1 Questao-2.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Rap do Minecraft\nRap do Minecraft\nRap do Minecraft\n\nRap do Minecraft\nRap do Minecraft\nRap do Minecraft\n\nEsse é o Creeper ele chegou para matar\nO Enderman muito louco, só vai teleportar\nA aranha parkourzeira chegou para te assustar\nE o esqueleto uma flecha no joelho vai atirar\n\nDesde a infdev o começo é sempre igual\nColetar madeira, fazer a casa inicial\nPicareta de madeira, é o ideal\nDepois é só pegar pedra, esse é o essencial\nA mineração sempre foi muito explorada\nNós vemos uma luz, e às vezes é dourada\nQuando pisamos e acende é avermelhada\nCadê o diamante? Vamos matar a charada\n\nRap do Minecraft\nRap do Minecraft\nRap do Minecraft\n\nRap do Minecraft\nRap do Minecraft\nRap do Minecraft\n\nAgora é a hora de pegarmos madeira\nEstamos cansados, quero ver nossa lareira\nSentamos e limpamos toda a sujeira\nHã, que sujeira? Isso é besteira\n\nChega de decorar, agora é hora de plantar\nPlantei umas sementes e elas já vão brotar\nSeed de melancia eu ainda vou encontrar\nMano, eu tô com uma preguiça de rimar\n\nEstamos com fome, tem um porco à vista\nVamos matar logo, não serei um egoísta\nA minha espada quebrou, modo boxista\nChega de rimar, porque eu tô com preguiça\n\nRap do Minecraft\nRap do Minecraft\nRap do Minecraft\n\nRap do Minecraft\nRap do Minecraft\nRap do Minecraft\n\nFinalmente dessa vez, nós vamos caçar\nUm Creeper muito chato dessa vez eu vou matar\nUm esqueleto camper conseguiu me acertar\nMas eu me escondi nessa caverna\nChega de fugir, eu vou me preparar\nCom armadura de diamante vou me equipar\nJá fiz a minha espada, agora vou encantar\nO meu arco-e-flecha dessa vez eu vou usar\n\nEu saio lá fora, um Enderman gigante\nDuas espadadas mato, seguirei avante\nOlha só 3 Creepers, que deselegante\nDepois dessa aventura tomo um chá, que relaxante\n\nRap do Minecraft\nRap do Minecraft\nRap do Minecraft\n\nRap do Minecraft\nRap do Minecraft\nRap do Minecraft\n");
+
+    return 0;
+}
