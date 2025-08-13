e105179f953d79fac06c3799d502c4289d0a289e
Afonso <afonsobranco34@outlook.com>
2025-08-13 20:14:41 -0300

    Sex

---
+++ Exercicio 5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/// Receba o raio e descubra a circunferencia kk vsf
+
+int main(void)
+{
+
+    float raio = 0;
+    float PI = 3.14;
+    float resultado = 0;
+
+    printf("Insira o Valor do raio para caucular a circunferencia.\n\nValor do Raio: ");
+    scanf("%f",&raio);
+
+    resultado = (PI * 2) * raio;
+
+    printf("%.2f",resultado);
+
+}
