ea3b6618186844b32abe67806c59c4272f7b3b33
Afonso <afonsobranco34@outlook.com>
2025-08-20 23:44:58 -0300

    a

--- Aula 1/Exercico-14.c
+++ Aula 1/Exercico-14.c
@@ -4,5 +4,15 @@ int main()
 {
     //​​Conversor de temperaturas (C ↔ F): escolha do tipo de conversão com if. 
 
+    char opção = 'q';
+
+    printf("Menu...\nPor favor escolha sua conversão[...]\n");
+
+    printf("A) Celsius ---> Farehaint\n");
+
+    printf("B) Farehanit ---> Celsius\n");
+
+    scanf(" %c",&opção);
+
     
 }
