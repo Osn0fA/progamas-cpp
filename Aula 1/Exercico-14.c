61343576710d81f4dc13c1d10a11d8432db43f4e
Afonso <afonsobranco34@outlook.com>
2025-08-21 21:15:50 -0300

    Sexo anal

--- Aula 1/Exercico-14.c
+++ Aula 1/Exercico-14.c
@@ -16,10 +16,31 @@ int main()
 
     if (opção == 'A' || opção == 'a'){
 
+        int celsius = 0, fahrenheit = 0;
+
+        printf("\n\n---> Coloque sua temperatura em Celsius aqui[...]\n\n--> ");
+
+        scanf("%d",&celsius);
+
+        fahrenheit = celsius * 1.8 + 32;
+
+        printf("\n\n Sua temperatura %d e igual a %d.",celsius,fahrenheit);
+
     }
 
     
     if (opção == 'B' || opção == 'b'){
+
+        int celsius = 0, fahrenheit = 0;
+
+        printf("\n\n---> Coloque sua temperatura em Fahrenheit aqui[...]\n\n--> ");
+
+        scanf("%d",&fahrenheit);
+
+        celsius = fahrenheit - 32;
+        celsius = celsius/1.8;
+
+        printf("\n\n Sua temperatura %d e igual a %d.",fahrenheit,celsius);
         
     }
     
